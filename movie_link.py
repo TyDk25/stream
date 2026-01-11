9625c51bdfa3d49479bbac7f5685dd8bb3d8f213
Tyrone <tyronedk00@hotmail.com>
2026-01-11 02:37:18 +0000

    Removed functionality for series to be searched without episode and season

--- movie_link.py
+++ movie_link.py
@@ -11,27 +11,34 @@ Retrieves movie or series information including streaming links.
 """
 
 
-def get_title(movie_name, series=False, season=None, episode=None) -> dict[str, str | Any]:
+def get_title(movie_name, season=None, episode=None) -> dict[str, str | Any]:
     result = movie_key.get_movie(movie_name)
     title = result['title']
     imdb_id = result['imdbid']
+    type = result['type']
 
-    if series:
+    if type == "series":
         if season is None or episode is None:
             raise ValueError("Season and Episode must be provided for series.")
         return {
             "title": title,
             "year": result.get("year", "N/A"),
+            "type": result.get("type", "series"),
             "imdb": f"https://www.imdb.com/title/{imdb_id}",
             "stream": f"https://vidsrc.me/embed/tv/{imdb_id}/{season}/{episode}"
         }
 
     else:
-        return {
+        if type == "movie":
+            return {
 
-            "title": title,
-            "year": result.get("year", "N/A"),
-            "type": result.get("type", "movie"),
-            "imdb": f"https://www.imdb.com/title/{imdb_id}",
-            "stream": f"https://vidsrc.me/embed/movie/{imdb_id}"
-        }
+                "title": title,
+                "year": result.get("year", "N/A"),
+                "type": result.get("type", "movie"),
+                "imdb": f"https://www.imdb.com/title/{imdb_id}",
+                "stream": f"https://vidsrc.me/embed/movie/{imdb_id}"
+
+            }
+
+
+print(get_title('inception'))
